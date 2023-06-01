23ab5190fe166d6b2d3372cb32e59195ed8f86ef
kcn3388 <kcn3388@126.com>
2023-06-01 10:38:30 +0800

    fix error

--- __init__.py
+++ __init__.py
@@ -5,42 +5,43 @@ from fgo_lib import *
 from fgo_fetch_all import *
 from download.download_icons import *
 
-warning_log = os.path.join(os.path.dirname(__file__), 'warning.log')
+if __name__ == "__main__":
+    warning_log = os.path.join(os.path.dirname(__file__), 'warning.log')
+    now = datetime.datetime.now()
 
-try:
-    get_all_mooncell()
-except Exception as e:
-    print(traceback.format_exc())
-    with open(warning_log, 'a', encoding='utf-8') as file:
-        file.write(f"{e}\n{traceback.format_exc()}\n")
-    pass
+    try:
+        get_all_mooncell()
+    except Exception as e:
+        print(traceback.format_exc())
+        with open(warning_log, 'a', encoding='utf-8') as file:
+            file.write(f"{now}\n{e}\n{traceback.format_exc()}\n")
+        pass
 
-try:
-    update_lib()
-except Exception as e:
-    print(traceback.format_exc())
-    with open(warning_log, 'a', encoding='utf-8') as file:
-        file.write(f"{e}\n{traceback.format_exc()}\n")
-    pass
+    try:
+        update_lib()
+    except Exception as e:
+        print(traceback.format_exc())
+        with open(warning_log, 'a', encoding='utf-8') as file:
+            file.write(f"{now}\n{e}\n{traceback.format_exc()}\n")
+        pass
 
-try:
-    down_all_card_res()
-except Exception as e:
-    print(traceback.format_exc())
-    with open(warning_log, 'a', encoding='utf-8') as file:
-        file.write(f"{e}\n{traceback.format_exc()}\n")
-    pass
+    try:
+        down_all_card_res()
+    except Exception as e:
+        print(traceback.format_exc())
+        with open(warning_log, 'a', encoding='utf-8') as file:
+            file.write(f"{now}\n{e}\n{traceback.format_exc()}\n")
+        pass
 
-try:
-    download_icons()
-except Exception as e:
-    print(traceback.format_exc())
-    with open(warning_log, 'a', encoding='utf-8') as file:
-        file.write(f"{e}\n{traceback.format_exc()}\n")
-    pass
+    try:
+        download_icons()
+    except Exception as e:
+        print(traceback.format_exc())
+        with open(warning_log, 'a', encoding='utf-8') as file:
+            file.write(f"{now}\n{e}\n{traceback.format_exc()}\n")
+        pass
 
-now = datetime.datetime.now()
-auto_upload = f"Auto upload in {now}\n"
-upload = os.path.join(os.path.dirname(__file__), 'auto_upload.md')
-with open(upload, 'w', encoding='utf-8') as file:
-    file.write(auto_upload)
+    auto_upload = f"Auto upload in {now}\n"
+    upload = os.path.join(os.path.dirname(__file__), 'auto_upload.md')
+    with open(upload, 'w', encoding='utf-8') as file:
+        file.write(auto_upload)
